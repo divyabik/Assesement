ebfd04f50bc482566f8f6e836a6b5b7b077a9d48
divyabik <118958389+divyabik@users.noreply.github.com>
2022-11-24 12:10:06 +0530

    Add files via upload

---
+++ 24-11-2022/code/HelloWorld.java
@@ -0,0 +1,8 @@
+class HelloWorld{
+	public static void main(String args[]){
+		System.out.println("NAME : DIVYA");
+		System.out.println("DEPARTMENT : BCA");
+		System.out.println("ADDRESS : CHENNAI");
+		
+	}
+}
